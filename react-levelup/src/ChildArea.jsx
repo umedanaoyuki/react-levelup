dc80e20d06ab58d9e634ee63a589e5a107281b4e
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-25 23:32:55 +0900

    レンダリング検証のための配列を準備（文字入力してもレンダリングされてしまう）

--- react-levelup/src/ChildArea.jsx
+++ react-levelup/src/ChildArea.jsx
@@ -7,6 +7,16 @@ const style = {
 
   
 export const ChildArea = ({open}) => {
+
+  console.log("ChildAreaがレンダリングされた");
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+  
+
+
   return (
     <>
       {open ? (<div style={style}><p>子コンポーネント</p></div>) : null}
