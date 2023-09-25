c53d8ac68781a9dcf4732a047ebca70ceb64ac01
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-25 23:29:02 +0900

    css

--- react-levelup/src/ChildArea.jsx
+++ react-levelup/src/ChildArea.jsx
@@ -1,7 +1,15 @@
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroudColor: "green"
+};
+
+  
 export const ChildArea = ({open}) => {
   return (
     <>
-      {open ? (<div><p>子コンポーネント</p></div>): null}
+      {open ? (<div style={style}><p>子コンポーネント</p></div>) : null}
     </>
   );
 };
