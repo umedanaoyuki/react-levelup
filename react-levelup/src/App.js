bdeb26c036d281901c5b34d70d430c30d98d9c62
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-26 00:12:42 +0900

    様々なCSS:　InlineStyle（React 純正）

--- react-levelup/src/App.js
+++ react-levelup/src/App.js
@@ -1,36 +1,14 @@
 import './App.css';
 import React, { useState, useCallback, useMemo } from 'react';
 import { ChildArea } from './ChildArea';
+import { InlineStyle } from './components/InlineStyle';
 
 
 function App() {
 
-  const [text, setText] = useState('');
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => {
-    console.log(e.target.value);
-    setText(e.target.value);
-  };
-
-  const onClickOpen = () => {
-    setOpen(!open);
-  };
-
-  const onClickClose = useCallback(() => {
-    setOpen(false); 
-  }, []);
-
-  const temp = useMemo(() => 1 +3, []);
-  console.log(temp);
-  
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText}/>
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose}/>
+      <InlineStyle />
     </div>
   );
 }
