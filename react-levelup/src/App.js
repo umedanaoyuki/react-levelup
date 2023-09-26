f7fcbc90815e77d5c66a135632f86427707ed3c2
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-26 23:02:27 +0900

    CSS modulesの書き方

--- react-levelup/src/App.js
+++ react-levelup/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { useState, useCallback, useMemo } from 'react';
 import { ChildArea } from './ChildArea';
 import { InlineStyle } from './components/InlineStyle';
+import CssModules from './components/CssModules';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
---
+++ react-levelup/src/components/CssModules.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './CssModules.module.scss'
+
+export const CssModules = () => {
+  return (
+    <div className={classes.container}>
+      <p>- CSS Modules -</p>
+      <button>FIGHT！</button>
+      </div>
+  )
+}
+
+export default CssModules
