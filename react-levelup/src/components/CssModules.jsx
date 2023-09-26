3d0612abed769ec137b6b63b33288dfbf9973cf3
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-26 23:08:26 +0900

    CSS modules完成

--- react-levelup/src/components/CssModules.jsx
+++ react-levelup/src/components/CssModules.jsx
@@ -4,8 +4,8 @@ import classes from './CssModules.module.scss'
 export const CssModules = () => {
   return (
     <div className={classes.container}>
-      <p>- CSS Modules -</p>
-      <button>FIGHT！</button>
+      <p className={classes.title}>- CSS Modules -</p>
+      <button className={classes.button}>FIGHT！</button>
       </div>
   )
 }
